13f7a64a4e6bb07485d128dc8144a148f46e6da0
Ahmed Saeed <ahmedsaeed0380@gmail.com>
2023-07-10 17:48:23 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# simple-ecommerce
+
+ # Technologies used:
+- React
+- React Router dom
+- Material UI
+
+## How to run
+
+Open your terminal to get clone this repo by running the following command:
+```bash
+git clone
+```
+Install the required packages and dependencies by running the following command:
+```bash
+yarn or npm install
+```
+Once the installation is complete, you can start the project by running the following command:
+```bash
+npm run or yarn start
+```
---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import HomeLayout from "./layout/homeLayout";
+import Count from "./component/Count";
+import { Container } from "@mui/system";
+import Box from "@mui/material/Box";
+import OurService from "./component/OurService";
+import OurSpecials from "./component/OurSpecials";
+import Products from "./component/Products/Products";
+import Subscribe from "./component/Subscrips";
+import LatestBlog from "./component/Blog/LatestBlog";
+import ContactUs from "./component/ContactUs";
+import OtherPosts from "./component/Blog/OtherPosts";
+import Footer from "./layout/Footer";
+function App() {
+  return (
+    <>
+      <HomeLayout />
+
+      <Container maxWidth={"lg"}>
+        <Count />
+
+        <Box sx={{ mt: 8, mb: 4 }}>
+          <OurService />
+        </Box>
+        <Box>
+          <OurSpecials />
+        </Box>
+
+        <Box>
+          <Products />
+        </Box>
+        <Box sx={{ mb: 8 }}>
+          <Subscribe />
+        </Box>
+        <Box>
+          <LatestBlog />
+        </Box>
+      </Container>
+      <Box>
+        <ContactUs />
+      </Box>
+
+      <Container>
+        <Box>
+          <OtherPosts />
+        </Box>
+      </Container>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
